84ccd52b41280cebcd91df5e060e50b24bb602d0
alihassancso <ali.hassan@csoutsource.com>
2025-08-22 02:01:03 +0500

    Update helloWorld.ts

--- inngest/helloWorld.ts
+++ inngest/helloWorld.ts
@@ -3,9 +3,8 @@ import { inngest } from "./client";
 export default inngest.createFunction(
   { id: "hello-world" },
   { event: "demo/event.sent" },
-  async ({ event, step }) => {
-    return {
-      message: `Hello ${event.name}!`,
-    };
+  async ({ event }) => {
+    // Your schema defines data.message; use that
+    return { message: `Hello ${event.data.message}!` };
   }
 );
